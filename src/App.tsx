e071956ccc698ac9e8e13d07b480db579b13c0af
richard150403 <richard.umoh@pau.edu.ng>
2021-05-04 12:18:57 +0100

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld4 from './compnents/HelloWorld4';
+import HelloWorld7 from './compnents/helloworld7/HelloWorld7';
+import HelloWorld8 from './compnents/HelloWorld8';
+import HelloWorld9 from './compnents/HelloWorld9';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="Column1">
+        <HelloWorld8 name="Pius" />
+      </div>
+      <div className="Column2">
+        <HelloWorld4 />
+      </div>
+      <div className="Column3">
+        <HelloWorld7 name="Peace" />
+      </div>
+      <div className="Column3">
+        <HelloWorld9 />
+      </div>
     </div>
   );
 }
